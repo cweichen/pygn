4f38659e7e297c29885c570dc4e94663b0b1983a
cweichen <chingweichen@gmail.com>
2013-10-19 13:36:46 +0800

    Added fetch by GNID method

--- pygn.py
+++ pygn.py
@@ -182,6 +182,103 @@ def search(clientID='', userID='', artist='', album='', track=''):
 		
 		return metadata
 
+def fetch(clientID='', userID='', GNID=''):
+	"""
+	Fetches a track or album by GN ID
+	"""
+
+	if clientID=='' or userID=='':
+		print 'ClientID and UserID are required'
+		return None
+
+	if GNID=='':
+		print 'GNID is required'
+		return None
+	
+	# Create XML request
+	query = _gnquery()
+
+	query.addAuth(clientID, userID)
+	query.addQuery('ALBUM_FETCH')
+	query.addQueryGNID(GNID)
+	query.addQueryOption('SELECT_EXTENDED', 'COVER,REVIEW,ARTIST_BIOGRAPHY,ARTIST_IMAGE,ARTIST_OET,MOOD,TEMPO')
+	query.addQueryOption('SELECT_DETAIL', 'GENRE:3LEVEL,MOOD:2LEVEL,TEMPO:3LEVEL,ARTIST_ORIGIN:4LEVEL,ARTIST_ERA:2LEVEL,ARTIST_TYPE:2LEVEL')
+	
+	queryXML = query.toString()
+	
+	if DEBUG:
+		print '------------'
+		print 'QUERY XML'
+		print '------------'
+		print queryXML
+	
+	# POST query
+	response = urllib2.urlopen(_gnurl(clientID), queryXML)
+	responseXML = response.read()
+	
+	if DEBUG:
+		print '------------'
+		print 'RESPONSE XML'
+		print '------------'
+		print responseXML
+
+	# Create GNTrackMetadata object
+	metadata = gnmetadata()
+	
+	# Parse response
+	responseTree = xml.etree.ElementTree.fromstring(responseXML)
+	responseElem = responseTree.find('RESPONSE')
+	if responseElem.attrib['STATUS'] == 'OK':
+	
+		# Find Album element
+		albumElem = responseElem.find('ALBUM')
+
+		# Parse album metadata
+		metadata['album_gnid'] = _getElemText(albumElem, 'GN_ID')
+		metadata['album_artist_name'] = _getElemText(albumElem, 'ARTIST')
+		metadata['album_title'] = _getElemText(albumElem, 'TITLE')
+		metadata['album_year'] = _getElemText(albumElem, 'DATE')
+		metadata['album_art_url'] = _getElemText(albumElem, 'URL', 'TYPE', 'COVERART')
+		metadata['genre'] = _getMultiElemText(albumElem, 'GENRE', 'ORD', 'ID')
+		metadata['artist_image_url'] = _getElemText(albumElem, 'URL', 'TYPE', 'ARTIST_IMAGE')
+		metadata['artist_bio_url'] = _getElemText(albumElem, 'URL', 'TYPE', 'ARTIST_BIOGRAPHY')
+		metadata['review_url'] = _getElemText(albumElem, 'URL', 'TYPE', 'REVIEW')
+	
+		# Look for OET
+		artistOriginElem = albumElem.find('ARTIST_ORIGIN')
+		if artistOriginElem is not None:
+			metadata['artist_origin'] = _getMultiElemText(albumElem, 'ARTIST_ORIGIN', 'ORD', 'ID')
+			metadata['artist_era'] = _getMultiElemText(albumElem, 'ARTIST_ERA', 'ORD', 'ID')
+			metadata['artist_type'] = _getMultiElemText(albumElem, 'ARTIST_TYPE', 'ORD', 'ID')
+		else:
+			# Try to get OET again by fetching album by GNID
+			metadata['artist_origin'], metadata['artist_era'], metadata['artist_type'] = _getOET(clientID, userID, metadata['album_gnid'])
+		
+		# Parse track metadata
+		matchedTrackElem = albumElem.find('MATCHED_TRACK_NUM')
+		if matchedTrackElem is not None:
+			trackElem = albumElem.find('TRACK')
+		
+			metadata['track_number'] = _getElemText(trackElem, 'TRACK_NUM')
+			metadata['track_gnid'] = _getElemText(trackElem, 'GN_ID')
+			metadata['track_title'] = _getElemText(trackElem, 'TITLE')
+			metadata['track_artist_name'] = _getElemText(trackElem, 'ARTIST')
+
+			metadata['mood'] = _getMultiElemText(trackElem, 'MOOD', 'ORD', 'ID')
+			metadata['tempo'] = _getMultiElemText(trackElem, 'TEMPO', 'ORD', 'ID')
+		
+			# If track-level GOET exists, overwrite metadata from album			
+			if trackElem.find('GENRE') is not None:
+				metadata['genre']	= _getMultiElemText(trackElem, 'GENRE', 'ORD', 'ID')
+			if trackElem.find('ARTIST_ORIGIN') is not None:
+				metadata['artist_origin'] = _getMultiElemText(trackElem, 'ARTIST_ORIGIN', 'ORD', 'ID')
+			if trackElem.find('ARTIST_ERA') is not None:
+				metadata['artist_era'] = _getMultiElemText(trackElem, 'ARTIST_ERA', 'ORD', 'ID')
+			if trackElem.find('ARTIST_TYPE') is not None:
+				metadata['artist_type'] = _getMultiElemText(trackElem, 'ARTIST_TYPE', 'ORD', 'ID')
+
+		return metadata
+
 def _gnurl(clientID):
 	"""
 	Helper function to form URL to Gracenote service
